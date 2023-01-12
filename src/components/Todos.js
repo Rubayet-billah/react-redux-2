da991131296fe1fd38d9c4ec35bc84746bc754cb
Rubayet-billah <rubayetbillah909@gmail.com>
2023-01-12 17:30:25 +0600

    posted

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllTodos } from '../services/actions/todosAction';
 
 const Todos = () => {
+    const { todos, isLoading, error } = useSelector(state => state)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getAllTodos)
+    }, [])
+
     return (
         <div>
-
+            <h1>Todos App</h1>
+            {isLoading && <h1>Loading...</h1>}
+            {error && <h1>{error.message}</h1>}
         </div>
     );
 };
