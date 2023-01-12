7247da273bdb3c5a07fb0a354e62d7f8f2191f6f
Rubayet-billah <rubayetbillah909@gmail.com>
2023-01-12 18:03:42 +0600

    style added

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllTodos } from '../services/actions/todosAction';
+import './Todos.css'
 
 const Todos = () => {
     const { todos, isLoading, error } = useSelector(state => state)
@@ -15,9 +16,18 @@ const Todos = () => {
             <h1>Todos App</h1>
             {isLoading && <h1>Loading...</h1>}
             {error && <h1>{error.message}</h1>}
-            {
-                todos?.map(todo => <div>{todo.title}</div>)
-            }
+            <main className='parent-container'>
+                {
+                    todos?.map(todo => {
+                        return <section key={todo.id}>
+                            <div className='child-container'>
+                                <p>{todo.id}</p>
+                                <p>{todo.title}</p>
+                            </div>
+                        </section>
+                    })
+                }
+            </main>
         </div>
     );
 };
